15dbf56aaa52a45fa3ab5903163efba8ef0d4dd2
Fernando Mota <106290886+Motadeveloper@users.noreply.github.com>
2024-08-12 20:56:42 -0300

    34345

--- youtube_downloader/urls.py
+++ youtube_downloader/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),  # Rota para a página inicial do app
 ]
