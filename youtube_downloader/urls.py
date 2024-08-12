3c437cc73cd5b6184e188dfb1d9e436797290fb2
Fernando Mota <106290886+Motadeveloper@users.noreply.github.com>
2024-08-12 20:14:03 -0300

    edit urls

--- youtube_downloader/urls.py
+++ youtube_downloader/urls.py
@@ -1,7 +1,8 @@
+# youtube_downloader/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('download/<str:nome_arquivo>/', views.download_audio, name='download_audio'),
+    path('download/<str:filename>/', views.download_audio, name='download_audio'),
 ]
