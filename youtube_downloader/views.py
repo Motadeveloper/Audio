e600ed2fa353be53c53509c6a37c4cca5ce41e34
Fernando Mota <106290886+Motadeveloper@users.noreply.github.com>
2024-08-12 20:07:42 -0300

    front 2

--- youtube_downloader/views.py
+++ youtube_downloader/views.py
@@ -1,8 +1,14 @@
 import os
+import random
+import string
 import yt_dlp
 from django.shortcuts import render
 from django.http import FileResponse, Http404
 from django.conf import settings
+
+def gerar_nome_aleatorio(extensao, tamanho=10):
+    caracteres = string.ascii_letters + string.digits
+    return ''.join(random.choice(caracteres) for _ in range(tamanho)) + '.' + extensao
 
 def baixar_video(url, formato):
     ydl_opts = {
@@ -17,8 +23,8 @@ def baixar_video(url, formato):
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
-        video_title = info.get('title', None)
-        video_filename = f"{video_title.replace(' ', '_').lower()}.{formato}"
+        video_title = info.get('title', 'video')
+        video_filename = gerar_nome_aleatorio(formato)
         os.rename(f"{video_title}.{formato}", video_filename)
 
     return video_title, video_filename
@@ -41,9 +47,7 @@ def index(request):
 def download_audio(request, video_filename):
     file_path = os.path.join(settings.BASE_DIR, video_filename)
     if os.path.exists(file_path):
-        response = FileResponse(open(file_path, 'rb'))
-        response['Content-Disposition'] = f'attachment; filename="{video_filename}"'
-        os.remove(file_path)
+        response = FileResponse(open(file_path, 'rb'), as_attachment=True)
         return response
     else:
         raise Http404("Arquivo não encontrado.")
