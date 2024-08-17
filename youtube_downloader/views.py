ab71979bd76afc6bae683d5c4c2af60d7cd96e90
Fernando Mota <106290886+Motadeveloper@users.noreply.github.com>
2024-08-16 23:01:57 -0300

    M.V.P.2.0.2

--- youtube_downloader/views.py
+++ youtube_downloader/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import yt_dlp
 import uuid
 import os
